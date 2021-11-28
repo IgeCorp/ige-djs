e150687d47865658ae96ddbf28e0febce0557161
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-11-28 12:04:52 +0100

    Update IgeSlash.js

--- src/IgeSlash.js
+++ src/IgeSlash.js
@@ -4,25 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Errrors_1 = __importDefault(require("./utils/Errrors"));
-/**
- * @example
- * ```js
- * const { IgeSlash } = require("@igecorp/ige-djs");
- *
- * class ping extends IgeSlash {
- *     constructor() {
- *         super({
- *             name: "ping",
- *             description: "Get the bot latency",
- *             type: "MESSAGE",
- *             options: null,
- *             defaultPermission: false
- *             guildOnly: true
- *         })
- *     }
- * }
- * ```
- */
 class IgeSlash {
     name;
     description;
@@ -31,7 +12,42 @@ class IgeSlash {
     defaultPermission;
     guildOnly;
     /**
+     * @external ApplicationCommandType
+     * @see {@link https://discord.js.org/#/docs/main/stable/typedef/ApplicationCommandType}
+     */
+    /**
+     * @external ApplicationCommandOptionType
+     * @see {@link https://discord.js.org/#/docs/main/stable/typedef/ApplicationCommandOptionType}
+     */
+    /**
+     * Slash command options choices
+     * @typedef {Array} SlashsCommandsOptionsChoices
+     * @property {string} name The name of the choice
+     * @property {string|number} value The value of the choice
+     */
+    /**
+     * Slash command options values
+     * @typedef {Array} SlashsCommandsOptions
+     * @property {ApplicationCommandOptionType} type The type of the option
+     * @property {string} name The name of the option
+     * @property {string} description The description of the option
+     * @property {boolean} required Whether the option is required
+     * @property {SlashsCommandsOptionsChoices[]} [choices=null] The choices of the option for the user to pick from
+     * @property {SlashsCommandsOptions} [options=null] Additional options if this option is a subcommand (group)
+     */
+    /**
+     * All slash command options
+     * @typedef {Object} SlashOptions
+     * @property {string} name The slash command name.
+     * @property {string} description The slash command description.
+     * @property {ApplicationCommandType} [type="CHAT_IMPUT"] The slash command type (CHAT_IMPUT, USER, MESSAGE).
+     * @property {SlashsCommandsOptions[]} [options=null] The slash command options (choices, ...)
+     * @property {boolean} [defaultPermission=true] The slash command defaultPermission.
+     * @property {boolean} [guildOnly=false] Boolean value if you want this command to one guild only.
+     */
+    /**
      * @param {SlashOptions} slashOptions The slash command options.
+     * @returns {Promise<IgeSlash>}
      */
     constructor(slashOptions) {
         if (!slashOptions)
